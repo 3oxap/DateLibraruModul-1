1e1c84dc905b3ec712fcd33914dd327fd8f08197
3oxap <62433200+3oxap@users.noreply.github.com>
2021-01-07 01:10:34 +0300

    Добавьте файлы проекта.

---
+++ DateLibraruModul-1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DateLibraryModule-1
+{
+    public class Class1
+    {
+        public static string Output(string name)
+        {
+            var line = $"Current time {DateTime.UtcNow.ToLongTimeString()}. Name {name}!";
+            return line;
+
+        }
+    }
+}
+
